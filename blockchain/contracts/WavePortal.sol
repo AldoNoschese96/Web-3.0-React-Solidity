0443586a8bcd138c3b73cf210471fce11f05ca68
AldoNoschese96 <aldo.noschese@webeetle.it>
2022-04-03 22:36:24 +0200

    Push solidity code

---
+++ blockchain/contracts/WavePortal.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    struct Wave {
+        address waver;
+        string text;
+        string author;
+        uint256 timestamp;
+    }
+
+    event NewWave(
+        address indexed from,
+        uint256 timestamp,
+        string text,
+        string author
+    );
+
+    uint256 private seed;
+    uint256 wavesCounter;
+    Wave[] wavesArray;
+
+    mapping(address => uint256) public lastWavedAt;
+
+    constructor() payable {
+        console.log("HELLO");
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function addWaveHandler(string memory _text, string memory _author) public {
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Wait 15m"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+        wavesArray.push(Wave(msg.sender, _text, _author, block.timestamp));
+        wavesCounter += 1;
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("Random # generated: %d", seed);
+
+        if (seed <= 50) {
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
+        emit NewWave(msg.sender, block.timestamp, _text, _author);
+    }
+
+    function getCountWaves() public view returns (uint256) {
+        return wavesCounter;
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return wavesArray;
+    }
+}
---
+++ blockchain/scripts/run.js
@@ -0,0 +1,60 @@
+const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  await waveContract.deployed();
+
+  /*
+   * Get Contract balance
+   */
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  let waveTxn = await waveContract.addWaveHandler("Ehy this is a Wave", "Aldo");
+  await waveTxn.wait();
+
+  /*
+   * Get Contract balance to see what happened!
+   */
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  // CONTRACT CONSUMPTION FROM OTHER WALLET
+  waveTxn = await waveContract
+    .connect(randomPerson)
+    .addWaveHandler("Ehy this is a new Wave", "Random Person");
+  await waveTxn.wait();
+
+  /*
+   * Get Contract balance to see what happened!
+   */
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0); // exit Node process without error
+  } catch (error) {
+    console.log(error);
+    process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+  }
+  // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+};
+
+runMain();
